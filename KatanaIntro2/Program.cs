d2c938d81a42a99f17208546dc1d55283ff7c102
Krutika <krutika@promactinfo.com>
2015-11-28 14:34:59 +0530

    changed

---
+++ KatanaIntro2/Program.cs
@@ -0,0 +1,20 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KatanaIntro2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public class startup
+        {
+            public void Configuration(IAppBuilder app) { }
+        }
+    }
+}
